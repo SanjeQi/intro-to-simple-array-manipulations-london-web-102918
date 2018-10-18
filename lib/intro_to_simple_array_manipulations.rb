29a65d155db5722138790de20ffcb012837977d8
bright-reducer-1915 <cristi.nord@gmail.com>
2018-10-18 14:05:40 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,5 @@
-def using_push(countries_in_western_africa, next_country)
- countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
- next_country = "Niger"
- countries_in_western_africa.push(next_country)
+def using_push(arr,string)
+  arr.push(string)
 end
 
 #def using_unshift(neighborhoods_in_northwest_brooklyn,new_neighborhood)
