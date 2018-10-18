8637f8e1c5aa0a521970528b36442b2c911dcbca
bright-reducer-1915 <cristi.nord@gmail.com>
2018-10-18 14:06:53 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,13 +1,12 @@
-def using_push(arr,string)
+def using_push(arr, string)
   arr.push(string)
 end
 
-#def using_unshift(neighborhoods_in_northwest_brooklyn,new_neighborhood)
- #  neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
-  # new_neighborhood = "Brooklyn Heights"
-   #neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
-#end
 
-def using_unshift(arr,string)
+def using_unshift(arr, string)
   arr.unshift(string)
 end
+
+def using_pop(arr)
+  arr.pop
+end  
